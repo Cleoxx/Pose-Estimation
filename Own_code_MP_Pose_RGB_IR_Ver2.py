aa460defb4645f7d56b936a6534fc1cab107088e
Cleoxx <104149085+Cleoxx@users.noreply.github.com>
2023-01-08 20:05:48 +0100

    Deleted MediaPipeVideo.py Fixed the Main code with the stop condition Fixed imshow of landmarked video Fixed out writer

--- Own_code_MP_Pose_RGB_IR_Ver2.py
+++ Own_code_MP_Pose_RGB_IR_Ver2.py
@@ -3,7 +3,7 @@
 # Main Code 
 # Computing and saving the lansmarks and difs into csv 
 
-import cv2
+import cv2 as cv
 import mediapipe as mp
 import numpy as np
 import os
@@ -18,18 +18,11 @@ import csv
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
-mp_pose = mp.solutions.pose    
-
-# For videos
-# Variables video file specific
-# Enter path to videos here
-#VID_FILES = [ r'C:\Users\User\Documents\Masterstudium\Masterarbeit\TestPics\Yoga-ein-Bein.jpg' ]
-#video_folder = "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\Thermoaufnahmen\\Skalierung 0-100"
-#video_file = "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\Thermoaufnahmen\\Skalierung 0-100\\GK_Squats_Seite_Front.ravi"
+mp_pose = mp.solutions.pose
 
 video_folder = "C:\\Users\\User\\Desktop\\VideosEdited"
-video_file_rgb = "C:\\Users\\User\\Desktop\\VideosEdited\\071222a.mp4"#ir071222ath.mp4
-video_file_ir = "C:\\Users\\User\\Desktop\\VideosEdited\\ir071222athBW.mp4" #ir071222ath.mp4"
+video_file_rgb = "C:\\Users\\User\\Desktop\\VideosEdited\\TEST_RGBa.mp4" #ir071222ath.mp4
+video_file_ir = "C:\\Users\\User\\Desktop\\VideosEdited\\TEST_IRa.mp4" #ir071222athBW.mp4"
 
 FrameDataAll = []
 rgb_frame_nr=0
@@ -58,6 +51,11 @@ def CSV_header_generation():
 
 writer = CSV_header_generation()
 
+#############################################
+#Output video to be saved and compared later 
+#############################################
+out = cv.VideoWriter('C:\\Users\\User\\Desktop\\VideosEdited\\RGBLandmarks.mp4', cv.VideoWriter_fourcc(*'avc1'), 20.0, (1920,1080))
+
 pose_rgb = mp_pose.Pose(
     static_image_mode=False,
     model_complexity=2,
@@ -80,90 +78,89 @@ pose = mp_pose.Pose(
     smooth_landmarks=True,
     min_tracking_confidence=0.5)
 
-# one selected video to test
-#video_location = video_file
-# output video path
-#out_vid_path = video_folder
-
 #import RGB video
-cap_rgb=cv2.VideoCapture(video_file_rgb)
-cap_ir=cv2.VideoCapture(video_file_ir)
+cap_rgb=cv.VideoCapture(video_file_rgb)
+cap_ir=cv.VideoCapture(video_file_ir)
 
 ## Setup mediapipe instance
-with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-    while cap_rgb.isOpened() or cap_ir.isOpened():
-        retrgb, frame = cap_rgb.read()
-        #cv2.imshow('Mediapipe Feed', frame)
-        ret_ir, frame_ir = cap_ir.read()
-        #cv2.imshow('Mediapipe Feed', frame_ir)
+pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+while cap_rgb.isOpened():
+    retrgb, frame = cap_rgb.read()
+    ret_ir, frame_ir = cap_ir.read()
+
+    #get frame width and height RGB
+    frame_width_rgb = int(cap_rgb.get(cv.CAP_PROP_FRAME_WIDTH))
+    frame_height_rgb = int(cap_rgb.get(cv.CAP_PROP_FRAME_HEIGHT))
+    length_rgb = int(cap_rgb.get(cv.CAP_PROP_FRAME_COUNT))
+    #get frame width and height IR
+    frame_width_ir = int(cap_ir.get(cv.CAP_PROP_FRAME_WIDTH))
+    frame_height_ir = int(cap_ir.get(cv.CAP_PROP_FRAME_HEIGHT))
+    length_ir = int(cap_ir.get(cv.CAP_PROP_FRAME_COUNT))
+    if rgb_frame_nr<length_rgb:
+        print(rgb_frame_nr)
+        print(length_rgb)
+        # Recolor image to RGB
+        rgb_frame_nr+=1
+        ir_frame_nr+=1
+
+        image_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        image_rgb.flags.writeable = False
+        image_ir = cv.cvtColor(frame_ir, cv.COLOR_BGR2RGB)
+        image_ir.flags.writeable = False
+
+        # Make detection RGB and IR
+        results_rgb = pose_rgb.process(image_rgb)
+        results_ir = pose_ir.process(image_ir)
+
+        # Recolor back to BGR
+        image_rgb.flags.writeable = True
+        image_rgb = cv.cvtColor(image_rgb, cv.COLOR_RGB2BGR)
         
-        #get frame width and height RGB
-        frame_width_rgb = int(cap_rgb.get(cv2.CAP_PROP_FRAME_WIDTH))
-        frame_height_rgb = int(cap_rgb.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        length_rgb = int(cap_rgb.get(cv2.CAP_PROP_FRAME_COUNT))
+        # Recolor back to BGR
+        image_ir.flags.writeable = True
+        image_ir = cv.cvtColor(image_ir, cv.COLOR_RGB2BGR)
+
+        # Extract landmarks
+        try:
+            landmarks_rgb=results_rgb.pose_landmarks.landmark
+            landmarks_ir = results_ir.pose_landmarks.landmark
+        except:
+            pass
+
+        # Render detections RGB
+        mp_drawing.draw_landmarks(image_rgb, results_rgb.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                    )  
+        # Render detections IR
+        mp_drawing.draw_landmarks(image_ir, results_ir.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                    )
+        #if rgb_frame_nr%100==0:
+        #    print(rgb_frame_nr, ir_frame_nr)
+        FrameDataAll.append(BodyParts(landmarks_rgb, landmarks_ir, writer))
+        #return VideoFileClip(video_folder
+        cv.rectangle(image_rgb, (0,0), (225,73), (245,117,16), -1)
         
-        #get frame width and height IR
-        frame_width_ir = int(cap_ir.get(cv2.CAP_PROP_FRAME_WIDTH))
-        frame_height_ir = int(cap_ir.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        length_ir = int(cap_ir.get(cv2.CAP_PROP_FRAME_COUNT))
-        
-        #Output video to be saved and compared later 
-        #out_rgb = cv2.VideoWriter('C:\\Users\\User\\Desktop\\VideosEdited\\LandmarkRGB.mp4',cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 50, (frame_width_rgb,frame_height_rgb))
-        
-        while rgb_frame_nr<length_rgb:
-            #cap_rgb.release()
-            # Recolor image to RGB
-            rgb_frame_nr+=1
-            ir_frame_nr+=1
-            image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            image_rgb.flags.writeable = False
-            image_ir = cv2.cvtColor(frame_ir, cv2.COLOR_BGR2RGB)
-            image_ir.flags.writeable = False
-      
-            # Make detection RGB and IR
-            results_rgb = pose_rgb.process(image_rgb)
-            results_ir = pose_ir.process(image_ir)
-    
-            # Recolor back to BGR
-            image_rgb.flags.writeable = True
-            image_rgb = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR)
-            # Recolor back to BGR
-            image_ir.flags.writeable = True
-            image_ir = cv2.cvtColor(image_ir, cv2.COLOR_RGB2BGR)
-        
-            # Extract landmarks
-            try:
-                landmarks_rgb=results_rgb.pose_landmarks.landmark
-                landmarks_ir = results_ir.pose_landmarks.landmark
-                #print(landmarks_rgb)
-                #out.write(image_rgb)
-            except:
-                pass
-        
-            # Render detections RGB
-            mp_drawing.draw_landmarks(image_rgb, results_rgb.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
-                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
-                                        )  
-            # Render detections IR
-            mp_drawing.draw_landmarks(image_ir, results_ir.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
-                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
-                                        )
+        # Rep data
+        cv.putText(image_rgb, 'FRAME', (15,12), 
+                cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv.LINE_AA)
+        cv.putText(image_rgb, str(rgb_frame_nr), 
+                (10,60), 
+                cv.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv.LINE_AA)
             
-            #BP_Difs = BodyParts(landmarks_rgb, landmarks_ir)
-            if rgb_frame_nr%100==0:
-                print(rgb_frame_nr, ir_frame_nr)
-            FrameDataAll.append(BodyParts(landmarks_rgb, landmarks_ir, writer))
-            #return VideoFileClip(video_folder
-            #cv2.imshow('Mediapipe Feed', image_rgb)
-            #cv2.imshow('Mediapipe Feed', image_ir)
-            #df=pd.DataFrame(dif_bp_dict[bp_string])
-        if cv2.waitKey(10) & 0xFF == ord('q'):
+        if retrgb:
+        #out.write(image_rgb)
+            out.write(image_rgb)
+            cv.imshow('Mediapipe Feed RGB', image_rgb)
+            #cv.imshow('Mediapipe Feed IR', image_ir)
+        if cv.waitKey(10) & 0xFF == ord('q'):
             break
-            
+    else:
         cap_rgb.release()
         cap_ir.release()
-    cv2.destroyAllWindows()
-    print(length_rgb, length_ir)
+out.release()
+cv.destroyAllWindows()
+print(length_rgb, length_ir)
 pprint.pprint(FrameDataAll)
