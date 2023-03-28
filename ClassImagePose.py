db4bd29d2db718d3539944517e608c807710b812
Cleoxx <104149085+Cleoxx@users.noreply.github.com>
2023-03-28 18:48:18 +0200

    Fixed ClassImagePose.py

--- ClassImagePose.py
+++ ClassImagePose.py
@@ -2,9 +2,8 @@ import mediapipe as mp
 import cv2
 import csv
 #import matplotlib.pyplot as plt
-
-
-
+#creating landmark csv from images
+#IMAGE_FILES = []
 class Image:
     body_parts = ('NOSE','LEFT_EYE_INNER','LEFT_EYE','LEFT_EYE_OUTER','RIGHT_EYE_INNER',
                 'RIGHT_EYE','RIGHT_EYE_OUTER','LEFT_EAR','RIGHT_EAR','MOUTH_LEFT','MOUTH_RIGHT','LEFT_SHOULDER',
@@ -13,11 +12,13 @@ class Image:
                 'LEFT_ANKLE','RIGHT_ANKLE','LEFT_HEEL','RIGHT_HEEL','LEFT_FOOT_INDEX','RIGHT_FOOT_INDEX')
     mp_pose = mp.solutions.pose
     mp_drawing = mp.solutions.drawing_utils
-    #mp_drawing_styles = mp.solutions.drawing_styles
+    mp_drawing_styles = mp.solutions.drawing_styles
+    
 
-    def __init__(self, spec, count, img_file, csvfile):
+    def __init__(self, spec, mod, length, img_file, csvfile):
         self.spec = spec
-        self.count = count
+        self.mod = mod
+        self.length = length
         self.img_file = img_file
         self.pose = Image.mp_pose.Pose(static_image_mode=True,
                                 model_complexity=2,
@@ -32,36 +33,38 @@ class Image:
         bp_csv_prep_fieldnames=[]
         csvfile = open(self.csvfile, 'w', newline='')
         for bp in Image.body_parts:
-            bp_csv_prep_fieldnames.extend([bp+"_x", bp+"_y", bp+"_z"])
-            #, bp+"_visibility"
+            bp_csv_prep_fieldnames.extend([bp+"_x", bp+"_y", bp+"_v"])
+            #, bp+"_z", bp+"_visibility"
         writer = csv.DictWriter(csvfile, fieldnames=bp_csv_prep_fieldnames)
         #print (bp_csv_prep_fieldnames)
         writer.writeheader()
         self.writer=writer
-        
-    def draw_landmarks(self, results, img):
-        self.mp_drawing.draw_landmarks(img, results.pose_landmarks, Image.mp_pose.POSE_CONNECTIONS,
+    def load_image(self):
+        #for idx, self.image_name in enumerate(self.img_file):
+        #for i in range (0,2):
+        self.image_name = self.img_file + '\\TrainImg_a_{}.jpg'.format(i)
+        self.img = cv2.imread(self.image_name)
+        print(self.img_file)
+        print(self.image_name)
+        #image_height, image_width, _ = self.img.shape
+        #i +=1
+        self.results = self.pose.process(self.img)
+        #self.results = self.pose.process(self.img)
+        #self.results = self.pose.process(cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB))
+        #self.length = len([IMAGE_FILES])
+        #return results
+    def draw_landmarks(self):
+        #self.annotated_image = self.img.copy()
+        self.mp_drawing.draw_landmarks(self.annotated_image, self.results.pose_landmarks, self.mp_pose.POSE_CONNECTIONS,
                                 self.mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
                                 self.mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
                                     )
-                                    
-    def get_img_specs(self):        
-        self.img_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.img_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        #self.length = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        #self.out = cv2.VideoWriter('C:\\Users\\User\\Desktop\\VideosEdited\\Output_Videos\\{}_Landmarks.mp4'.format(self.spec),
-                                    #cv2.VideoWriter_fourcc(*'avc1'), 20.0, 
-                                    #(self.frame_width,self.frame_height))
-        
-    def image_detection(self, img):#, frame
-        self.image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self.image.flags.writeable = False
-        results = self.pose.process(img)
-        self.image.flags.writeable = True
-        self.image = cv2.cvtColor(self.image, cv2.COLOR_RGB2BGR)
-        return results
-    def extract_landmarks(self, results):
-        landmarks=results.pose_landmarks.landmark
+        #self.mp_drawing.draw_landmarks(
+        #self.annotated_image, self.results.pose_landmarks,
+        #self.mp_pose.POSE_CONNECTIONS,
+        #landmark_drawing_spec=self.mp_drawing_styles.get_default_pose_landmarks_style())
+    def extract_landmarks(self):
+        landmarks=self.results.pose_landmarks.landmark
         return landmarks
     def writerow(self, landmarks):
         bp_dict={}
@@ -69,65 +72,75 @@ class Image:
             bp=getattr(Image.mp_pose.PoseLandmark, bp_string)
             bp_dict[bp_string+"_x"]=landmarks[bp.value].x
             bp_dict[bp_string+"_y"]=landmarks[bp.value].y
-            bp_dict[bp_string+"_z"]=landmarks[bp.value].z
+            #bp_dict[bp_string+"_z"]=landmarks[bp.value].z
+            bp_dict[bp_string+"_v"]=landmarks[bp.value].visibility
             #self.plot([landmarks[bp.value].x, landmarks[bp.value].y, landmarks[bp.value].z])
         self.writer.writerow(bp_dict)
-    def plot(self, pos):
-        # WORK IN PROGRESS 
-        figure = plt.figure()
-        ax = figure.add_subplot(111, projection = "3d")
-        ax.plot(pos[0], pos[1], pos[2])
-        figure.show()
-        print(pos)
-        deleteme = input()
-    def draw_frame_counter(self, frame, frame_nr):
-        cv2.rectangle(frame, (0,0), (225,73), (245,117,16), -1)
-        cv2.putText(frame, 'FRAME', (15,12), 
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-        cv2.putText(frame, str(frame_nr), (10,60), 
-                cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
-    def show_write_img(self, img):
+    def show_write_img(self, i):
         #self.out.write(frame)
-        self.out = cv2.imwrite('C:\\Users\\User\\Desktop\\VideosEdited\\Output_Images\\{}_Landmarks.png'.format(self.spec))
-        cv2.imshow('Mediapipe Feed', img)
-    def close(self):
-        #self.out.release()
-        cv2.destroyAllWindows()
+        self.out = cv2.imwrite('C:/Users/User/Desktop/VideosEdited/Output_Images/{}_{}_Landmarks.png'.format(self.spec, i), self.annotated_image)
+        #cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
+        cv2.imshow('Mediapipe Feed', self.annotated_image)
+        #Image.mp_drawing.plot_landmarks(self.results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
     def process_Image(self):
-        self.get_img_specs()
-        i=0
-        for i in range (0,count):
-            i+=1
-            results = self.image_detection(img)
-            self.draw_landmarks(results, img)
+        #self.length = 10
+        for i in range (0, self.length):
+            self.image_name = self.img_file + '\\TrainImg_{}_{}.jpg'.format(self.mod, i)
+            self.img = cv2.imread(self.image_name)
+            self.results = self.pose.process(self.img)
+            self.annotated_image = self.img.copy()
+            #self.load_image()
+            self.draw_landmarks()
             try:
-                landmarks = self.extract_landmarks(results)
+                landmarks = self.extract_landmarks()
                 self.writerow(landmarks)
             except Exception as e:
                 print (e)
-                self.writerow(0)
                 pass
-            if ret:
-                self.show_write_img(frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-            else:
-                self.cap.release()
-        self.close()
-        #print(length)
+            self.out = cv2.imwrite('C:/Users/User/Desktop/VideosEdited/Output_Images/{}_{}_Landmarks.png'.format(self.spec, i), self.annotated_image)
+            #cv2.imshow('Mediapipe Feed', self.annotated_image)
+            i += 1
         
-    
-    
+        #return i
+        #cv2.destroyAllWindows()
 
 
+a_artistic_image = Image('artistic', 'a', 134,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_artisticxy.csv")
+a_artistic_image.process_Image()
+b_artistic_image = Image('artistic', 'b', 117,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_artisticxy.csv")
+b_artistic_image.process_Image()
+c_artistic_image = Image('artistic', 'c', 91,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_artisticxy.csv")
+c_artistic_image.process_Image()
+d_artistic_image = Image('artistic', 'd', 82,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_artisticxy.csv")
+d_artistic_image.process_Image()
+e_artistic_image = Image('artistic', 'e', 59,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_artisticxy.csv")
+e_artistic_image.process_Image()
+f_artistic_image = Image('artistic', 'f', 74,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_artisticxy.csv")
+f_artistic_image.process_Image()
 
+a_norm_image = Image('norm', 'a', 134,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1\\bandw', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_normimagexy.csv")
+a_norm_image.process_Image()
+b_norm_image = Image('norm', 'b', 117,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1\\bandw', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_normimagexy.csv")
+b_norm_image.process_Image()
+c_norm_image = Image('norm', 'c', 91,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1\\bandw', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_normimagexy.csv")
+c_norm_image.process_Image()
+d_norm_image = Image('norm', 'd', 82,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1\\bandw', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_normimagexy.csv")
+d_norm_image.process_Image()
+e_norm_image = Image('norm', 'e', 59,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1\\bandw', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_normimagexy.csv")
+e_norm_image.process_Image()
+f_norm_image = Image('norm', 'f', 74,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1\\bandw', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_normimagexy.csv")
+f_norm_image.process_Image()
 
-c_image = Image('c', 91, 'C:/Users/User/Documents/Masterstudium/Masterarbeit/TestPics/train_data1/bandw/TrainImg_c_{}.jpg'.format(i), "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_c.csv")
+a_rgb_image = Image('rgb', 'a', 134,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_rgbimagexy.csv")
+a_rgb_image.process_Image()
+b_rgb_image = Image('rgb', 'b', 117,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_rgbimagexy.csv")
+b_rgb_image.process_Image()
+c_rgb_image = Image('rgb', 'c', 91,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_rgbimagexy.csv")
+c_rgb_image.process_Image()
+d_rgb_image = Image('rgb', 'd', 82,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_rgbimagexy.csv")
+d_rgb_image.process_Image()
+e_rgb_image = Image('rgb', 'e', 59,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_rgbimagexy.csv")
+e_rgb_image.process_Image()
+f_rgb_image = Image('rgb', 'f', 74,'C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\TestPics\\train_data1', "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_rgbimagexy.csv")
+f_rgb_image.process_Image()
 
-
-
-
-#Video_RGB = Video('RGB', "C:\\Users\\User\\Desktop\\VideosEdited\\071222a.mp4", "C:\\Users\\User\\Documents\\Masterstudium\\Masterarbeit\\Pose-Estimation_own\\Landmark CSV\\Landmarks_RGB.csv")#071222a.mp4, TEST_RGBa.mp4
-
-
-#Video_RGB.process_Image()
